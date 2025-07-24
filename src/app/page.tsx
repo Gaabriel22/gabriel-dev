3bc03e8a932584716a7070812f6d383c7105bf02
Gabriel Amaral <gabrielvieira2205@gmail.com>
2025-07-24 12:50:54 -0300

    feat: estrutura página inicial com seções de header, hero, sobre, projetos, serviços e contato

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+import Footer from "@/components/common/Footer"
+import Header from "@/components/common/Header"
+import About from "@/components/home/About"
+import Contact from "@/components/home/Contact"
+import Hero from "@/components/home/Hero"
+import Projects from "@/components/home/Projects"
+import Services from "@/components/home/Services"
+
 export default function Page() {
   return (
     <>
-      <h1>Hello, Next.js!</h1>
+      <Header />
+      <Hero />
+      <About />
+      <Projects />
+      <Services />
+      <Contact />
+      <Footer />
     </>
   )
 }
